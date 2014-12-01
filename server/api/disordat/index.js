4b4401e1e88abeaa533a46fd46ce8af3201d2f84
Els Dessin <els.dessin@gmail.com>
2014-12-01 10:40:57 +0100

    updated server to retrieve bad or good answer

--- server/api/disordat/index.js
+++ server/api/disordat/index.js
@@ -7,4 +7,8 @@ var router = express.Router();
 
 router.get('/question', controller.question);
 
+router.get('/answers', controller.answers);
+
+router.post('/answer', controller.answer);
+
 module.exports = router;
