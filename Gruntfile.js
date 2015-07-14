0c025bef609a03daacdeb6662ba55db436addb90
Els Dessin <els.dessin@gmail.com>
2015-07-14 21:34:11 +0200

    removed jshint-stylish

--- Gruntfile.js
+++ Gruntfile.js
@@ -121,8 +121,9 @@ module.exports = function (grunt) {
     // Make sure code styles are up to par and there are no obvious mistakes
     jshint: {
       options: {
-        jshintrc: '<%= yeoman.client %>/.jshintrc',
-        reporter: require('jshint-stylish')
+        jshintrc: '<%= yeoman.client %>/.jshintrc'
+		//,
+        //reporter: require('jshint-stylish')
       },
       server: {
         options: {
